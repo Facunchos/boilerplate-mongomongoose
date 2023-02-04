297477e95b27634ab115d7bbcb329a9a387bac80
*Facundo* <*facundoezequielmartinez@gmail.com*>
2023-02-04 19:26:33 -0300

    Schema mongoose

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+  
+});
 
-let Person;
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
